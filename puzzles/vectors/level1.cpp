e1288bb6209dfd274995d43e71864a23013fea0c
jtnachbar <nachbarjamie@gmail.com>
2018-01-04 14:34:14 -0500

    Update level1.cpp

--- puzzles/vectors/level1.cpp
+++ puzzles/vectors/level1.cpp
@@ -2,7 +2,7 @@
 
 float myPos[3];
 float itemPos[3];
-float relativePosition[3];
+float relativePos[3];
 
 myPos[0] = 0.1f;
 myPos[1] = 0.2f;
@@ -12,9 +12,7 @@ itemPos[0] = -0.1f;
 itemPos[1] = -0.1f;
 itemPos[2] = -0.1f;
 
-relativePosition[0] = itemPos[0] - myPos[0];
-relativePosition[1] = itemPos[1] - myPos[1];
-relativePosition[2] = itemPos[2] - myPos[2];
+mathVecSubtract(relativePos, itemPos, myPos, 3);
 
-DEBUG(("relativePosition: %f %f %f", relativePosition[0],
-relativePosition[1], relativePosition[2]));
+DEBUG(("relativePosition: %f %f %f", relativePos[0],
+relativePos[1], relativePos[2]));

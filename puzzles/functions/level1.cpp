112cbef97776356e402bc7ce216196c10a21a62e
MeisterNewman <stephenharmonnewman@gmail.com>
2018-01-04 15:21:01 -0500

    Create level1.cpp

---
+++ puzzles/functions/level1.cpp
@@ -0,0 +1,8 @@
+//What will this code return?
+int a(int number1, int number2){
+    return (number1*number2+1);
+}
+int b(int number1, int number2){
+    return (number1-number2);
+}
+DEBUG(("%i",a(b(1,2),b(3,4)) ));

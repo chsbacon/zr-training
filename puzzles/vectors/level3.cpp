1f5efe64180a0b80796ecd3db8b4db4d2a9768f6
jtnachbar <nachbarjamie@gmail.com>
2018-01-04 15:10:51 -0500

    Update level3.cpp

--- puzzles/vectors/level3.cpp
+++ puzzles/vectors/level3.cpp
@@ -4,12 +4,12 @@ float firstvec[3];
 float secondvec[3];
 float resultvec[3];
 
-firstvec[0] = 1;
+firstvec[0] = .5;
 firstvec[1] = 0;
 firstvec[2] = 0;
 
 secondvec[0] = 0;
-secondvec[1] = 1;
+secondvec[1] = .5;
 secondvec[2] = 0;
 
 mathVecCross(resultvec, firstvec, secondvec);

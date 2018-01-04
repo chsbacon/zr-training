6a6e574a2cfda6bea99b814f8f742cfd12d79c04
jonahweissman <19804455+jonahweissman@users.noreply.github.com>
2018-01-04 14:09:04 -0500

    Create level1.cpp

---
+++ puzzles/control_flow/level1.cpp
@@ -0,0 +1,19 @@
+// What will this program debug?
+
+int numPickups = 1;
+int maxPickups = 3;
+int numDrills = 5;
+int maxDrills = 17;
+bool justDrilled = false;
+
+if(numPickups > maxPickups or justDrilled){
+    DEBUG(("Back to base"));
+    if(maxDrills > numDrills){
+        DEBUG(("We gucci fam"))
+    }
+}
+
+if(numPickups < maxDrills and justDrilled){
+    DEBUG(("Geyser hit"))
+}
+
